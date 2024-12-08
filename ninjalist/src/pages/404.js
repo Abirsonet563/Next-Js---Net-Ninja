589e9a38a5228d6cfe49913d30e9710cfe26e91d
Abir Hasan <abirsonet563@gmail.com>
2024-12-08 22:52:56 +0000

    Redrirecting User - 8

--- ninjalist/src/pages/404.js
+++ ninjalist/src/pages/404.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 function NotFound() {
-  return (
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(()=>{
+//  router.go(1)
+    router.push('/')
+        }, 3000)
+         }, [])
+
+    return (
     <div className='not-found'> 
     <h1>oppps.....</h1>
     <h1>That page is not found.</h1>
@@ -10,4 +21,4 @@ function NotFound() {
   )
 }
 
-export default NotFound
+export default NotFound;
