afd758b01e3a8294b7a0be5461c5c8792d57cb8e
Abir Hasan <abirsonet563@gmail.com>
2024-12-09 03:53:26 +0000

    Dynamic Routes - 11

--- ninjalist/src/pages/ninja/index.js
+++ ninjalist/src/pages/ninja/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from '../../styles/Ninjas.module.css'
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -14,11 +15,13 @@ const ninja = ({ ninjas }) => {
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map(ninja => (
-        <div key={ninja.id}>
-          <a className={styles.single}>
+        <Link href={'/ninja/' + ninja.id} 
+        className={styles.single} 
+        key={ninja.id}>
+     
             <h3>{ninja.name}</h3>
-          </a>
-        </div>
+       
+        </Link>
       ))}
     </div>
   );
