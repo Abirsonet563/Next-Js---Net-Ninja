0703b1acaaba16055c43b75aee22c9c1baefc1e1
Abir Hasan <abirsonet563@gmail.com>
2024-12-09 03:37:57 +0000

    Fetching Data - 11

--- ninjalist/src/pages/ninja/index.js
+++ ninjalist/src/pages/ninja/index.js
@@ -1,10 +1,27 @@
-const ninja = () => {
-    return ( 
-        <div>
-        <h1>Ninja</h1>
-        <p>LOL</p>
+import styles from '../../styles/Ninjas.module.css'
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const ninja = ({ ninjas }) => {
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
         </div>
-     );
+      ))}
+    </div>
+  );
 }
  
 export default ninja;
