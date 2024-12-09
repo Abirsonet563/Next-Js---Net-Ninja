b726140dc8fa86067f645956f7af5263a4524911
Abir Hasan <abirsonet563@gmail.com>
2024-12-09 04:38:02 +0000

    Fetching a Single Items

--- ninjalist/src/pages/ninja/[id].js
+++ ninjalist/src/pages/ninja/[id].js
@@ -1,4 +1,4 @@
-export const getStaticPath = async() => {
+export const getStaticPaths = async() => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
 
@@ -13,10 +13,25 @@ export const getStaticPath = async() => {
     }
    
 }
-const details = () => {
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await res.json();
+
+    return{
+        props: {ninja: data}
+    }
+}
+const details = ({ninja}) => {
     return ( 
         <div>
             <h1>Details Page</h1>
+            <h1>{ninja.name}</h1>
+            <p>{ninja.email}</p>
+            <p>{ninja.website}</p>
+            <p>{ninja.address.city}</p>
+
         </div>
      );
 }
